f7587693a6ee575e51a86199917c2aaacded6ec8
SamLolo <sam.e.townley@icloud.com>
2026-01-22 10:47:47 +0000

    Start working with individual bounding boxes for each LSOA

--- data.py
+++ data.py
@@ -1,4 +1,33 @@
+import math
+import subprocess
 from pathlib import Path
 
 DATA_DIR = Path(__file__).parent / 'data'
 OUT_DIR = Path(__file__).parent / 'out'
+TEMP_DIR = Path(__file__).parent / 'temp'
+
+def get_osm_extract(
+    id: str,
+    long: float,
+    lat: float,
+    radius: float,
+):
+    dlat = radius / 111_000
+    dlon = radius / (111_000 * math.cos(math.radians(lat)))
+
+    bbox = (
+        long - dlon,
+        lat - dlat,
+        long + dlon,
+        lat + dlat,
+    )
+
+    cmd = [
+        "osmium", "extract",
+        "--bbox", f"{bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]}",
+        DATA_DIR / "raw" / "england-260119.osm.pbf",
+        "-o", TEMP_DIR / f"{id}.osm.pbf",
+        "--overwrite",
+    ]
+
+    subprocess.run(cmd, check=True)
--- isochrones.py
+++ isochrones.py
@@ -1,11 +1,58 @@
-import r5py
-import geopandas as gpd
-from data import DATA_DIR
+import os
+import pyproj
+import platform
 
-BATCH_SIZE = 1000
+os.environ["PROJ_LIB"] = pyproj.datadir.get_data_dir()
+
+# Change AppData folder to avoid issues with file access permissions on windows
+#if platform.system() == "Windows":
+#    os.environ["LOCALAPPDATA"] = "C:/"
+    
+import r5py
+
+import time
+import geopandas as gpd
+from data import DATA_DIR, TEMP_DIR, get_osm_extract
+from datetime import datetime, timedelta
+from poc.spatial_join import get_devon_lsoas
+
+BATCH_SIZE = 100
+
+# Load centriods
+centriods = get_devon_lsoas()
+centriods.reset_index(inplace=True)
+origin = centriods.loc[0]
+print(origin)
+lat = origin['geometry'].y
+long = origin['geometry'].x
+print(lat, long)
+
+start_time = time.time()
+
+get_osm_extract(origin['id'], origin['geometry'].x, origin['geometry'].y, 70000)
+
+print(f"Cropped OSM: {round(time.time() - start_time, 2)}s")
+start_time = time.time()
 
 # Create transport network for England
 transport_network = r5py.TransportNetwork(
-    osm_pbf = DATA_DIR / "raw" / "england-260119.osm.pbf",
-    gtfs = [DATA_DIR / "processed" / "england_gtfs_clean.zip"]
+    osm_pbf = TEMP_DIR / f'{origin['id']}.osm.pbf',
+    gtfs = [DATA_DIR / ".." / "poc" / "data" / "itm_south_west_gtfs.zip"]
 )
+
+print(f"Generated transport network: {round(time.time() - start_time, 2)}s")
+start_time = time.time()
+
+bus = r5py.Isochrones(
+    transport_network,
+    origins=origin['geometry'],
+    departure=datetime(2026, 1, 19, 8, 30),
+    departure_time_window=timedelta(hours=1),
+    transport_modes=[r5py.TransportMode.TRANSIT, r5py.TransportMode.WALK],
+    point_grid_resolution=100,
+    isochrones=[40]
+)
+
+print(f"Created isochrone: {round(time.time() - start_time, 2)}s")
+
+print(bus)
