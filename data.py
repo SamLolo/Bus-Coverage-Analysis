7467130249e61eaeaa2a00dc804ff3fd8fa911be
SamLolo <sam.e.townley@icloud.com>
2026-01-22 12:31:36 +0000

    Experiment with grouping LSOAs by MSOAs to reduce number of transport networks required

--- data.py
+++ data.py
@@ -1,30 +1,28 @@
-import math
 import subprocess
+import geopandas as gpd
 from pathlib import Path
+from pyproj import Transformer
+from shapely.ops import transform
 
 DATA_DIR = Path(__file__).parent / 'data'
 OUT_DIR = Path(__file__).parent / 'out'
 TEMP_DIR = Path(__file__).parent / 'temp'
 
-def get_osm_extract(
-    id: str,
-    long: float,
-    lat: float,
-    radius: float,
-):
-    dlat = radius / 111_000
-    dlon = radius / (111_000 * math.cos(math.radians(lat)))
+def get_osm_extract(id: str, gdf: gpd.GeoDataFrame, radius: float):
+    dissolved_geometry = gdf.dissolve().at[0, 'geometry']
+    
+    to_meters = Transformer.from_crs("EPSG:4326", "EPSG:3857", always_xy=True).transform
+    to_degrees = Transformer.from_crs("EPSG:3857", "EPSG:4326", always_xy=True).transform
 
-    bbox = (
-        long - dlon,
-        lat - dlat,
-        long + dlon,
-        lat + dlat,
-    )
+    dissolved_geometry = transform(to_meters, dissolved_geometry)
+    buffered = dissolved_geometry.buffer(radius)
+    dissolved_geometry = transform(to_degrees, buffered)
+    
+    min_long, min_lat, max_long, max_lat = dissolved_geometry.bounds
 
     cmd = [
         "osmium", "extract",
-        "--bbox", f"{bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]}",
+        "--bbox", f"{min_long},{min_lat},{max_long},{max_lat}",
         DATA_DIR / "raw" / "england-260119.osm.pbf",
         "-o", TEMP_DIR / f"{id}.osm.pbf",
         "--overwrite",
--- isochrones.py
+++ isochrones.py
@@ -7,52 +7,66 @@ os.environ["PROJ_LIB"] = pyproj.datadir.get_data_dir()
 # Change AppData folder to avoid issues with file access permissions on windows
 #if platform.system() == "Windows":
 #    os.environ["LOCALAPPDATA"] = "C:/"
-    
-import r5py
 
+import r5py
 import time
+import pandas as pd
 import geopandas as gpd
 from data import DATA_DIR, TEMP_DIR, get_osm_extract
 from datetime import datetime, timedelta
-from poc.spatial_join import get_devon_lsoas
 
 BATCH_SIZE = 100
+MSOAID = "E02004156"
 
-# Load centriods
-centriods = get_devon_lsoas()
-centriods.reset_index(inplace=True)
-origin = centriods.loc[0]
-print(origin)
-lat = origin['geometry'].y
-long = origin['geometry'].x
-print(lat, long)
+# Load MSOAs
+msoas = gpd.read_file(DATA_DIR / "raw" / "Middle_layer_Super_Output_Areas_December_2021_Boundaries_EW_BSC_V3_-6267947188164534400.gpkg", use_arrow=True)
+
+# Convert to Lat/Long CRS to match centriods CRS
+msoas.to_crs("EPSG:4326", inplace=True)
+EXETER = msoas[msoas['MSOA21CD'] == MSOAID]
+
+# Get all LSOA centriods in England
+centriods = gpd.read_file(DATA_DIR / "processed" / "LSOA_Centres.gpkg", use_arrow=True)
+
+# Find centriods that lie inside the region boundary using a spatial join
+lsoas = centriods.sjoin(EXETER, how="inner")
+lsoas.reset_index(inplace=True)
+print(lsoas)
 
 start_time = time.time()
 
-get_osm_extract(origin['id'], origin['geometry'].x, origin['geometry'].y, 70000)
+get_osm_extract(MSOAID, lsoas, 70000)
 
 print(f"Cropped OSM: {round(time.time() - start_time, 2)}s")
 start_time = time.time()
 
 # Create transport network for England
 transport_network = r5py.TransportNetwork(
-    osm_pbf = TEMP_DIR / f'{origin['id']}.osm.pbf',
+    osm_pbf = TEMP_DIR / f'{MSOAID}.osm.pbf',
     gtfs = [DATA_DIR / ".." / "poc" / "data" / "itm_south_west_gtfs.zip"]
 )
 
 print(f"Generated transport network: {round(time.time() - start_time, 2)}s")
 start_time = time.time()
 
-bus = r5py.Isochrones(
-    transport_network,
-    origins=origin['geometry'],
-    departure=datetime(2026, 1, 19, 8, 30),
-    departure_time_window=timedelta(hours=1),
-    transport_modes=[r5py.TransportMode.TRANSIT, r5py.TransportMode.WALK],
-    point_grid_resolution=100,
-    isochrones=[40]
-)
+isochrones = []
+for index, lsoa in lsoas.iterrows():
 
-print(f"Created isochrone: {round(time.time() - start_time, 2)}s")
+    bus = r5py.Isochrones(
+        transport_network,
+        origins=lsoa['geometry'],
+        departure=datetime(2026, 1, 19, 8, 30),
+        departure_time_window=timedelta(hours=1),
+        transport_modes=[r5py.TransportMode.TRANSIT, r5py.TransportMode.WALK],
+        point_grid_resolution=100,
+        isochrones=[40]
+    )
+    
+    bus['id'] = lsoa['id']
+    isochrones.append(bus.copy())
 
-print(bus)
+buses = pd.concat(isochrones)
+buses.drop("travel_time", axis=1, inplace=True)
+
+print(f"Created {lsoas.shape[0]} isochrones: {round(time.time() - start_time, 2)}s")
+print(buses)
