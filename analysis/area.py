86e6cc46b6747ae25f45160a69d5168bef9749cb
SamLolo <sam.e.townley@icloud.com>
2026-02-18 11:34:21 +0000

    Create script to calculate area-based accessibility indicator

---
+++ analysis/area.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import geopandas as gpd
+from common.data import OUT_DIR
+
+# Load completed isochrones
+bus = gpd.read_file(OUT_DIR / "bus_isochrones_combined.gpkg", use_arrow=True)
+car = gpd.read_file(OUT_DIR / "car_isochrones_combined.gpkg", use_arrow=True)
+
+# Convert to cylindrical equal-area format 
+bus.to_crs({'proj':'cea'}, inplace=True) 
+car.to_crs({'proj':'cea'}, inplace=True) 
+
+# Combine df's on ID
+area_df: gpd.GeoDataFrame = pd.merge(bus, car, on="id")
+
+# Calculate area in square km's
+area_df['bus_area'] = area_df['geometry_x'].area / 10**6
+area_df['car_area'] = area_df['geometry_y'].area / 10**6
+
+# Calculate ratio of bus to car area
+area_df['ratio'] = (area_df['bus_area'] / area_df['car_area'])
+
+# Clean up dataframe
+area_df.rename({"name_x": "name"}, inplace=True)
+area_df.drop(["name_y", "geometry_x", "geometry_y"], axis=1, inplace=True)
+
+# Save to file
+area_df.to_file(OUT_DIR / "areas.gpkg", driver="GPKG", use_arrow=True)
