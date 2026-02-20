13690f49b9cadd40d17d09fa1da71042093e363d
SamLolo <sam.e.townley@icloud.com>
2026-02-20 11:51:04 +0000

    Create a small script based off poc to visualise individual lsoa isochrones

---
+++ plotting/visualise.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import geopandas as gpd
+from common.data import OUT_DIR
+
+# Define LSOA ID to plot
+TARGET_LSOA = "E01027452"
+
+# Load isochrones
+bus = gpd.read_file(OUT_DIR / "bus_isochrones_combined.gpkg")
+car = gpd.read_file(OUT_DIR / "car_isochrones_combined.gpkg")
+
+# Set type to serve as key on map
+bus['type'] = "Bus + Walking"
+car['type'] = "Driving"
+
+# Combine isochrones into single dataframe
+combined = pd.concat([bus, car])
+combined = combined[combined['id'].str.contains(TARGET_LSOA)]
+
+# Create interactive map and save as html file to explore
+map = combined.explore(column="type", cmap=["blue", "purple"])
+map.save(OUT_DIR / f"{TARGET_LSOA}_isochrones.html")
