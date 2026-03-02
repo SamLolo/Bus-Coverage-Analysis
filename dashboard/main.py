4c15e3034c636ddcb29ce3ac38e88e3363fef414
SamLolo <sam.e.townley@icloud.com>
2026-03-02 14:16:07 +0000

    Started development of dashboard using flask

---
+++ dashboard/main.py
@@ -0,0 +1,11 @@
+from .app import IsochroneApp
+from flask import render_template
+
+app = IsochroneApp(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
