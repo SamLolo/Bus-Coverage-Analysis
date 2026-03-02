71af1beff68f2d72f4f54a45b7bdbb951fb78a62
SamLolo <sam.e.townley@icloud.com>
2026-03-02 16:52:42 +0000

    Remove lsoas who have an area ratio >1 post calculation

--- analysis/area.py
+++ analysis/area.py
@@ -8,12 +8,12 @@ car = gpd.read_file(OUT_DIR / "car_isochrones_combined.gpkg", use_arrow=True)
 print("Loaded isochrones")
 
 # Convert to cylindrical equal-area format 
-bus.to_crs({'proj':'cea'}, inplace=True) 
-car.to_crs({'proj':'cea'}, inplace=True)
+bus_cea = bus.to_crs({'proj':'cea'}) 
+car_cea = car.to_crs({'proj':'cea'})
 print("Converted to cylindrical equal-area")
 
 # Combine df's on ID
-area_df: gpd.GeoDataFrame = pd.merge(bus, car, on="id")
+area_df: gpd.GeoDataFrame = pd.merge(bus_cea, car_cea, on="id")
 print("Combined dataframes")
 
 # Calculate area in square km's
@@ -30,6 +30,22 @@ area_df.rename({"name_x": "name"}, axis=1, inplace=True)
 area_df.drop(["name_y", "geometry_x", "geometry_y"], axis=1, inplace=True)
 print("Cleaned dataframe")
 
+# Remove lsoas with area ratios > 1 as these are considered invalid
+invalid_ratios = bus[bus['id'].isin(area_df[area_df['ratio'] > 1]['id'])].copy()
+area_df = area_df[area_df['ratio'] <= 1]
+bus = bus[~bus['id'].isin(invalid_ratios['id'])]
+car = car[~car['id'].isin(invalid_ratios['id'])]
+print("Removed lsoas with ratio > 1")
+
+# Add to previous invalid_lsoas list
+invalid = gpd.read_file(OUT_DIR / "invalid_lsoas.gpkg")
+invalid_ratios['reason'] = "Area ratio is greater than 1"
+invalid = pd.concat([invalid, invalid_ratios])
+print("Add removed lsoas to 'invalid_lsoas.gpkg'")
+
 # Save to file
 area_df.to_csv(OUT_DIR / "areas.csv")
-print("Saved to file")
+bus.to_file(OUT_DIR / "bus_isochrones_combined.gpkg", driver="GPKG", use_arrow=True)
+car.to_file(OUT_DIR / "car_isochrones_combined.gpkg", driver="GPKG", use_arrow=True)
+invalid.to_file(OUT_DIR / "invalid_lsoas.gpkg", driver="GPKG", use_arrow=True)
+print("Saved to files")
