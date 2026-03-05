a7db95ce7521ea410ed901dc3d539278fa93a2d3
SamLolo <sam.e.townley@icloud.com>
2026-03-05 20:37:50 +0000

    Completed Moran's I calculations using new data, and fix some issues un-aligned data giving previously invalid results

---
+++ analysis/spatial_correlation.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import geopandas as gpd
+from libpysal.weights import Queen
+from esda.moran import Moran, Moran_Local
+from common.data import OUT_DIR, load_dataset, Datasets
+
+# Load LSOA boundaries
+lsoas = load_dataset(Datasets.LSOA_BOUNDARIES)
+removed = gpd.read_file(OUT_DIR / "invalid_lsoas.gpkg", use_arrow=True)
+lsoas = lsoas[~lsoas['id'].isin(removed['id'])]
+print("Loaded LSOA boundaries")
+
+# Load other spatial indicators
+areas = pd.read_csv(OUT_DIR / "areas.csv")
+destinations = pd.read_csv(OUT_DIR / "destination_totals.csv")
+print("Loaded accessibility indicator calculations")
+
+# Merge areas to create single dataframe
+lsoas = pd.merge(lsoas, areas, how="left", on="id")
+lsoas.drop(["Unnamed: 0", "name_y"], axis=1, inplace=True)
+lsoas.rename({"name_x": "name", "ratio": "areas_ratio"}, axis=1, inplace=True)
+
+# Merge destinations to create single dataframe
+lsoas = pd.merge(lsoas, destinations, how="left", on="id")
+lsoas.drop(["Unnamed: 0", "name_y"], axis=1, inplace=True)
+lsoas.rename({"name_x": "name", "ratio": "destinations_ratio"}, axis=1, inplace=True)
+print("Created single master dataframe")
+
+# Remove Isles of Scilly as it's spatially seperate
+lsoas = lsoas[lsoas['id'] != "E01019077"]
+print("Removed 'E01019077: ISLES OF SCILLY' as it's spatially seperate")
+
+# Create spatial weights matrix
+weights = Queen.from_dataframe(lsoas, ids="id")
+weights.set_transform("R")
+print("Created weights matrix")
+
+# Which indicators to use for calculations
+indicators = {
+    "bus_area": lsoas['bus_area'],
+    "car_area": lsoas['car_area'],
+    "areas_ratio": lsoas['areas_ratio'],
+    "employment": lsoas['employment_ratio'],
+    "education": lsoas['education_ratio'],
+    "healthcare": lsoas['healthcare_ratio'],
+    "bus_destinations": lsoas['total_bus'],
+    "car_destinations": lsoas['total_car'],
+    "destinations_ratio": lsoas['destinations_ratio']
+}
+
+# Calculate global Moran's I
+global_results = pd.DataFrame(columns=["indicator", "morans_i", "p-test"])
+for i, key in enumerate(indicators.keys()):
+    morans = Moran(indicators[key], weights)
+    global_results.loc[i] = [key, morans.I, morans.p_sim]
+    
+# Save global results to file
+global_results.to_csv(OUT_DIR / "correlations" / "global_correlations.csv")
+print("Calculated global Moran's I")
+
+# Calculaye LISA correlation for each indicator
+for name, values in indicators.items():
+    results = lsoas[["id", "name"]].copy()
+
+    # Calculate local Moran's I
+    lisa = Moran_Local(values, weights)
+    results["morans_i"] = lisa.Is
+    results["p-value"] = lisa.p_sim
+    
+    # Determine clustering by looking at permuation test
+    clustering = []
+    for i in range(len(lisa.q)):
+        if lisa.p_sim[i] >= 0.05:
+            clustering.append("not significant")
+        else:
+            match lisa.q[i]:
+                case 1:
+                    clustering.append("high-high")
+                case 2:
+                    clustering.append("low-high")
+                case 3:
+                    clustering.append("low-low")
+                case 4:
+                    clustering.append("high-low")
+    
+    # Save results dataframe to csv file  
+    results["clustering"] = clustering
+    results.to_csv(OUT_DIR / "correlations" / f"{name}_correlations.csv")
+    print("Calculated LISA values for indicator:", name)
