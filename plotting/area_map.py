3984732a29984c7dec7b94098b0b032980503476
SamLolo <sam.e.townley@icloud.com>
2026-03-06 11:43:09 +0000

    Update area map with labels and colorbar

---
+++ plotting/area_map.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from common.data import OUT_DIR, load_dataset, Datasets
+
+# Load LSOA boundaries
+lsoas = load_dataset(Datasets.LSOA_BOUNDARIES)
+
+# Load areas and merge with lsoa boundaries
+areas = pd.read_csv(OUT_DIR / "areas.csv")
+areas: gpd.GeoDataFrame = lsoas.merge(areas, on="id")
+
+# Create map plot
+areas.plot(column="ratio", 
+           cmap="viridis", 
+           vmin=np.percentile(areas['ratio'], 10), 
+           vmax=np.percentile(areas['ratio'], 90), 
+           legend=True,
+           legend_kwds={"label": "Bus/Car Area Ratio"})
+
+# Turn axis off
+plt.axis("off")
+plt.tight_layout()
+
+# Save to png
+plt.savefig(OUT_DIR / "plots" / "area_map.png", dpi=300)
