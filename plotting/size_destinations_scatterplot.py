240c43a2dbcffe28657a7aaeb299255f712cc414
SamLolo <sam.e.townley@icloud.com>
2026-03-09 22:29:39 +0000

    Create scatterplot of isochrone size vs number of destinations

---
+++ plotting/size_destinations_scatterplot.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from common.data import OUT_DIR
+
+# Load areas and destination results
+areas = pd.read_csv(OUT_DIR / "areas.csv")
+destinations = pd.read_csv(OUT_DIR / "destination_totals.csv")
+
+# Create single dataframe
+results = pd.merge(areas, destinations, on="id")
+
+# Plot each onto a scattergraph with different colours
+plt.scatter(results['car_area'], results['total_car'], color="blue", alpha=0.5, s=10, label="Driving")
+plt.scatter(results['bus_area'], results['total_bus'], color="red", alpha=0.5, s=10, label="Bus + Walking")
+
+# Add title and labels
+plt.title('How Isochrone Size Affects Destinations')
+plt.xlabel('Size of Isochrone (km²)')
+plt.ylabel('Number of Destinations')
+
+# Add legend
+plt.legend(loc="upper left")
+
+# Save to png
+plt.savefig(OUT_DIR / "plots" / "size_destinations_scatterplot.png", dpi=600)
