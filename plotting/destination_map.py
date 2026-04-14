819c6e1dc6d11b6648189a0273b968feeb0a332a
SamLolo <sam.e.townley@icloud.com>
2026-04-14 13:05:15 +0100

    Add some statistics about the destinations ratio

--- plotting/destination_map.py
+++ plotting/destination_map.py
@@ -11,6 +11,16 @@ lsoas = load_dataset(Datasets.LSOA_BOUNDARIES)
 destinations = pd.read_csv(OUT_DIR / "destinations" / "totals.csv")
 destinations: gpd.GeoDataFrame = lsoas.merge(destinations, on="id")
 
+# Print out some statistics
+print("\nRATIO:")
+print(f"Mean: {destinations['ratio'].mean():.4f}")
+print(f"Median: {destinations['ratio'].median():.4f}")
+print(f"Range: {(destinations['ratio'].max() - destinations['ratio'].min()):.4f}")
+print(f"Std: {destinations['ratio'].std():.4f}")
+print(f"Above 0.05: {(destinations['ratio'] >= 0.05).mean() * 100:.1f}%")
+print(f"Above 0.10: {(destinations['ratio'] >= 0.1).mean() * 100:.1f}%")
+print(f"Above 0.50: {(destinations['ratio'] >= 0.5).mean() * 100:.1f}%")
+
 # Create ticks
 ticks = [round(n, 3) for n in np.linspace(np.percentile(destinations['ratio'], 5), np.percentile(destinations['ratio'], 95), 4)]
 
